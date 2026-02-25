c4887ed6f1a6e7766000b56e7fe253e51b95c4ab
ilanding <testnewpremium@gmail.com>
2026-02-25 23:46:06 +0530

    server.js

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Correct static folder
-app.use(express.static(path.join(__dirname)));
+// Static files serve
+app.use(express.static(__dirname));
 
-// Force root route
+// Root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Start server (last me)
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
